4070982fdee3a213e9d1de0fbad2d532f25a8104
sahraomar <sahraaliwrites@gmail.com>
2024-07-24 03:31:47 -0400

    commit 1

--- library.js
+++ library.js
@@ -1,1 +1,72 @@
 // Your code goes here
+const { MongoClient, ObjectId } = require("mongodb");
+
+class Library {
+  constructor(dbUrl, dbName, collName) {
+    this.dbUrl = dbUrl;
+    this.dbName = dbName;
+    this.collName = collName;
+    this.dbClient;
+  }
+  // method to connect to Mongo server
+  async client() {
+    console.log(`Connecting to ${this.dbUrl}...`);
+    this.dbClient = new MongoClient(this.dbUrl, {});
+    await this.dbClient.connect();
+    console.log("Connected to database");
+    return this.dbClient;
+  }
+
+  // method to test connection
+  async test() {
+    const client = await this.client();
+    // add test operators here
+    client.close();
+    console.log("Connection closed");
+  }
+  // method to get collection
+  async collection() {
+    const client = await this.client();
+    const db = client.db(this.dbName);
+    const collection = db.collection(this.collName);
+    return collection;
+  }
+  // method to retrieve  all books
+  async allBooks() {
+    const collection = await this.collection();
+    return collection.find({}).toArray();
+  }
+  // find one book by its id
+  async findOneBook(id) {
+    const docId = new ObjectId(id);
+    const collection = await this.collection();
+    return collection.findOne({ _id: docId });
+  }
+  // find multiple books
+  async findManyBooks(query) {
+    const collection = await this.collection();
+    return collection.find(query).toArray();
+  }
+  //add a new book to collection
+  async addBook(info) {
+    const collection = await this.collection();
+    const result = await collection.insertOne(info);
+    console.log("Book is added successfully", result.insertId);
+  }
+  //update book id with new information
+  async changeBook(id, newInfo) {
+    const mongoId = new ObjectId(id);
+    const infoObj = { $set: newInfo };
+    const collection = await this.collection();
+    const result = await collection.updateOne({ _id: mongoId }, infoObj);
+    console.log("Book is updated successfully");
+  }
+  // remove book by its id
+  async removeBook(id) {
+    const mongoId = new ObjectId(id);
+    const collection = await this.collection();
+    const result = await collection.deleteOne({ _id: mongoId });
+    console.log("Book is removed successfully", result.deletedCount);
+  }
+}
+module.exports = Library;
--- server.js
+++ server.js
@@ -1,1 +1,54 @@
-// Your code goes here
+const express = require("express");
+const Library = require("./library");
+
+const app = express();
+const port = 3000;
+
+//Middleware to parse JSON and URL encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Mongo connection details
+const url = "mongodb://localhost:27017/";
+const dbName = "library";
+const collectionName = "books";
+
+//create a new instance of the Library class
+const collection = new Library(url, dbName, collectionName);
+
+// get route to fetch all books
+app.get("/bringmeallthestuff", async (req, res) => {
+  try {
+    const allBooks = await collection.allBooks();
+    res.json(allBooks);
+  } catch (error) {
+    res.status(500).send("Error fetching books: " + error.message);
+  }
+});
+
+// POST route to add a new book
+app.post("/addnewstuff", async (req, res) => {
+  try {
+    const { title, author, copies } = req.body;
+    await collection.addBook({ title, author, copies });
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send("Error adding book: " + error.message);
+  }
+});
+
+// POST route to update a book
+app.post("/updatestuff", async (req, res) => {
+  try {
+    const { id, newInfo } = req.body;
+    await collection.changeBook(id, JSON.parse(newInfo));
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send("Error updating book: " + error.message);
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
